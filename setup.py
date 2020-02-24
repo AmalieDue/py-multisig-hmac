789369a0ae98c7b2dd8ded8180ed57b8d29f3b54
Amalie Due Jensen <amalieduejensen@hotmail.com>
2020-02-24 10:13:55 +0100

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='multisig-hmac',  
-    version='0.0.4',
+    version='0.2.1',
     author="Amalie Due Jensen",
     author_email="amalieduejensen@hotmail.com",
     description="multisig HMAC",
